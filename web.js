9db35a349890aea646ead64f61ca6cde1d40be4b
Bryan King <goldbrickin@gmail.com>
2013-07-22 01:45:35 +0000

    Mod web.js again3

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var html = fs.readFileSync('index.html');
 var buffer = new Buffer(html, "utf-8");
 
 app.get('/', function(request, response) {
-  response.send(buffer.write());
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
